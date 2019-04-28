de12aa136ab4efbc3a5ca2de3e6bccefcb762c06
Arnold Oduma <arnoldcliff98@gmail.com>
2019-04-28 10:38:26 +0300

    Linked the html input fieldds to the javascript and assigned them to variables

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Declaring the variables
-var valueOne, valueTwo, valueThree, output;
+var valueOne, valueTwo, valueThree, output, sides;
+
+
+function findTriangle(){
+
+  valueOne = parseInt(document.getElementById("numberOne").value);
+  valueTwo = parseInt(document.getElementById("numberTwo").value);
+  valueThree = parseInt(document.getElementById("numberThree").value);
+  output = document.getElementById("output");
+
+  sides = [valueOne, valueTwo, valueThree]
+
+ 
 
 
 
+}
