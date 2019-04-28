4d20425320463b0e18bfffb5b4f404fb9c24c6c0
Arnold Oduma <arnoldcliff98@gmail.com>
2019-04-28 13:01:03 +0300

    REmoved the alerts and made the triangle names to be displayed inside the html element with id output

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var valueOne, valueTwo, valueThree, output, sides;
 
 function findTriangle() {
 
+  // variables to hold the values from the form
   valueOne = parseInt(document.getElementById("numberOne").value);
   valueTwo = parseInt(document.getElementById("numberTwo").value);
   valueThree = parseInt(document.getElementById("numberThree").value);
@@ -15,18 +16,22 @@ function findTriangle() {
   // alert("You entered " + sides);
   // alert(Math.max(parseInt(sides)));
 
+
+  // if else statements to determine the triangle type
   if (sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]) {
-    alert("The values you entered cannot form a triangle");
+    output.innerHTML = "The values you entered cannot form a triangle";
+    output.className = 'red';
+
   } 
   else if (sides[0] === sides[1] && sides[2] === sides[1]) {
-    // alert("This is an equilateral triangle");
     output.innerHTML = "Equilateral triangle";
+    output.className = '';
   } 
   else if (sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2]) {
-    alert("Isosceles");
+    output.innerHTML = "Isosceles triangle";
   } 
   else if (valueOne !== valueTwo !== valueThree) {
-    alert("Scalene");
+    output.innerHTML = "Scalene triangle";
   } 
   else {
     alert("Make sure you enter numbers");
