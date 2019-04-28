94d1f50feb7b62d5d000a6bd4b9a584dd44c589d
Arnold Oduma <arnoldcliff98@gmail.com>
2019-04-28 12:04:54 +0300

    Added if else statements to determine whether the values entered form a triangle and what type of triangle it is

--- js/script.js
+++ js/script.js
@@ -2,17 +2,35 @@
 var valueOne, valueTwo, valueThree, output, sides;
 
 
-function findTriangle(){
+function findTriangle() {
 
   valueOne = parseInt(document.getElementById("numberOne").value);
   valueTwo = parseInt(document.getElementById("numberTwo").value);
   valueThree = parseInt(document.getElementById("numberThree").value);
   output = document.getElementById("output");
 
+  // Created an array to hold the entered  values.
   sides = [valueOne, valueTwo, valueThree]
 
- 
+  // alert("You entered " + sides);
+  // alert(Math.max(parseInt(sides)));
 
+  if (sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0]) {
+    alert("The values you entered cannot form a triangle");
+  } 
+  else if (sides[0] === sides[1] && sides[2] === sides[1]) {
+    // alert("This is an equilateral triangle");
+    output.innerHTML = "Equilateral triangle";
+  } 
+  else if (sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2]) {
+    alert("Isosceles");
+  } 
+  else if (valueOne !== valueTwo !== valueThree) {
+    alert("Scalene");
+  } 
+  else {
+    alert("Make sure you enter numbers");
 
+  } 
 
-}
+  }
